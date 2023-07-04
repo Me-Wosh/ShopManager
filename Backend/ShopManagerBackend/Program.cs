32d85dd2eeb3b0ca1b6656dc87d6cd9d5c85403d
mewosh <milosz.samotyjak@gmail.com>
2023-07-04 21:13:42 +0200

    Enabled Patch Requests With Content

--- Backend/ShopManagerBackend/Program.cs
+++ Backend/ShopManagerBackend/Program.cs
@@ -20,7 +20,9 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173");
+            policy.WithOrigins("http://localhost:5173")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
         });
 });
 
