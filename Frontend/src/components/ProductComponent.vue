7479e5da9f91f2fa8e9bdf702dbc5d669efb1708
mewosh <milosz.samotyjak@gmail.com>
2023-07-04 21:36:45 +0200

    Products summary, No-Image Icon
    Added products summary which keeps track of quantities that were updated, blocks buttons if there are no products to update and blocks Mark As Sold button if there exists a product which quantity will be larger than previously (because when you sell an item it is logical that its quantity cant go up), backend functionality that differentiates these buttons to be implemented in the future. Added a default image which displays if product has no image. Removed Hat.svg from components folder

--- Frontend/src/components/ProductComponent.vue
+++ Frontend/src/components/ProductComponent.vue
@@ -2,21 +2,27 @@
   export default {
     name: "ProductComponent",
     
-    props: ['name', 'price', 'quantity', 'imagePath']
+    props: ['name', 'price', 'quantity', 'imagePath'],
+    
+    data () {
+      return {
+        image: 'src/' + this.imagePath
+      }
+    }
   }
 </script>
 
 <template>
   <div class="pane">
-    <img :src="'src/'+imagePath" :alt="name + ' image'"/>
+    <img :src="image" @error="this.image = 'src/assets/No-Image.svg'" :alt="name + ' image'"/>
     <div class="namePrice">
-    <p>{{ name }}</p>
-    <p>${{ price }}</p>
+      <p class="name">{{ name }}</p>
+      <p class="price">${{ price }}</p>
     </div>
-    <div class="quantity">
-      <i class="bi bi-dash"></i>
-      {{ quantity }}
-      <i class="bi bi-plus"></i>
+    <div class="quantityContainer">
+      <i @click="this.$emit('reduce-quantity')" class="bi bi-dash changeQuantityLeft"></i>
+      <p class="quantity">{{ quantity }}</p>
+      <i @click="this.$emit('increase-quantity')" class="bi bi-plus changeQuantityRight"></i>
     </div>
   </div>
 </template>
@@ -29,7 +35,8 @@
     min-height: 100px;
     width: 150px;
     margin: 0.5em;
-    border: 1px solid blue;
+    border: 1px solid #6394e3;
+    border-radius: 10px;
   }
   
   img {
@@ -39,19 +46,59 @@
     padding: 0.5em;
   }
   
+  img::before {
+    background-image: url("@/assets/No-Image.svg");
+  }
+  
   .namePrice {
     display: flex;
-    justify-content: space-between;
+    flex-direction: column;
     padding: 0.5em;
+  }
+  
+  .name {
+    font-size: 1.2em;
+    align-self: center;
+    word-wrap: break-word;
+  }
+  
+  .price {
+    align-self: center;
   }
   
   p {
     margin: 0;
     padding: 0;
   }
+
+  .quantityContainer {
+    display: flex;
+    justify-content: center;
+    align-self: center;
+    margin-bottom: 0.5em;
+    border: 1px solid gray;
+    border-radius: 5px;
+  }
   
   .quantity {
+    display: flex;
+    justify-content: center;
     align-self: center;
-    padding: 0.5em;
+    min-width: 1.7em;
+    font-size: 1.2em;
+    
+  }
+  
+  .changeQuantityLeft, .changeQuantityRight {
+    font-size: 2em;
+    align-self: center;
+  }
+  
+  .changeQuantityLeft {
+    border-right: 1px solid gray;
+  }
+  
+  .changeQuantityRight {
+    border-left: 1px solid gray;
   }
 </style>
--- Frontend/src/views/HomeView.vue
+++ Frontend/src/views/HomeView.vue
@@ -11,13 +11,14 @@
     
     data() {
       return {
-        products: [{id: 1, imagePath: "assets/Hat.svg", name: "Hat", price: 16.9, quantity: 40, sku: "HX421RO"},
-          {id: 2, imagePath: "assets/Hoodie.svg", name: "Hoodie", price: 16.9, quantity: 40, sku: "HX421RO"},
-          {id: 3, imagePath: "assets/Jacket.svg", name: "Jacket", price: 16.9, quantity: 40, sku: "HX421RO"},
-          {id: 4, imagePath: "assets/Pants.svg", name: "Pants", price: 16.9, quantity: 40, sku: "HX421RO"},
-          {id: 5, imagePath: "assets/Shirt.svg", name: "Shirt", price: 16.9, quantity: 40, sku: "HX421RO"},
-          {id: 6, imagePath: "assets/Shoes.svg", name: "Sweater", price: 16.9, quantity: 40, sku: "HX421RO"}
-        ]
+        products: [{id: -1, imagePath: "assets/Hat.svg", name: "A suspiciously long name for a regular hat", price: 99.9, quantity: 1000000, sku: "HX421RO"},
+          {id: -2, imagePath: "assets/Hoodie.svg", name: "Hoodie", price: 16.9, quantity: 40, sku: "HX421RO"},
+          {id: -3, imagePath: "assets/Jacket.svg", name: "Jacket", price: 99999.9, quantity: 40, sku: "HX421RO"},
+          {id: -4, imagePath: "assets/Pants.svg", name: "Pants", price: 16.9, quantity: 1, sku: "HX421RO"},
+          {id: -5, imagePath: "assets/Shirt.svg", name: "Shirt", price: 16.9, quantity: 40, sku: "HX421RO"},
+          {id: -6, imagePath: "assets/Shoes.svg", name: "Shoes", price: 16.9, quantity: 40, sku: "HX421RO"}],
+        
+        productsToUpdate: []
       }
     },
     
@@ -26,8 +27,51 @@
         axios.get('https://localhost:7020/Api/Products/V1/Available')
             .then((response) => {
               this.products = response.data
-              console.log(this.products)
             })
+      },
+
+      updateQuantities() {
+        if (this.productsToUpdate.length < 1)
+          return
+        
+        axios({
+          method: 'patch',
+          url: 'https://localhost:7020/Api/Products/V1/UpdateQuantities',
+          data: this.productsToUpdate
+        })
+        
+        this.productsToUpdate.length = 0
+      },
+      
+      reduceQuantity(product) {
+        let index = this.productsToUpdate.findIndex(p => p.id === product.id)
+        
+        if (index !== -1) {
+          if (product.quantity > 0) {
+            this.productsToUpdate[index].quantity--
+            product.quantity--
+          }
+          if (this.productsToUpdate[index].quantity === 0)
+            this.productsToUpdate.splice(index, 1)
+        } else {
+          this.productsToUpdate.push({id: product.id, name: product.name, quantity: -1})
+          product.quantity--
+        }
+      },
+
+      increaseQuantity(product) {
+        let index = this.productsToUpdate.findIndex(p => p.id === product.id)
+
+        if (index !== -1) {
+          this.productsToUpdate[index].quantity++
+          product.quantity++
+          
+          if (this.productsToUpdate[index].quantity === 0)
+            this.productsToUpdate.splice(index, 1)
+        } else {
+          this.productsToUpdate.push({id: product.id, name: product.name, quantity: 1})
+          product.quantity++
+        }
       }
     }
   }
@@ -56,7 +100,29 @@
           :price="product.price"
           :quantity="product.quantity"
           :image-path="product.imagePath"
+          @reduce-quantity="reduceQuantity(product)"
+          @increase-quantity="increaseQuantity(product)"
           />
+        </div>
+        <div class="productsSummary">
+          <div class="summary">
+            <p>Summary</p>
+          </div>
+          
+          <div class="productsToUpdate">
+            <div v-for="product in productsToUpdate">
+              {{product.name}} {{product.quantity > 0 ? '+' + product.quantity : product.quantity}}
+            </div>
+          </div>
+        </div>
+        <div class="productActions">
+          <button :disabled="productsToUpdate.length < 1" @click="updateQuantities">
+            <i title="Modify quantities" class="bi bi-tools"></i>
+          </button>
+          
+          <button :disabled="productsToUpdate.length < 1 || productsToUpdate.some(p => p.quantity > 0)" @click="updateQuantities">
+            <i title="Mark as sold" class="bi bi-bag sold"></i>
+          </button>
         </div>
       </div>
     </div>
@@ -67,13 +133,13 @@
   .window {
     display: flex;
     justify-content: center;
-    
+    min-height: 100vh;
+    min-width: 100vw; /* fix .grid hiding */
   }
 
   .main {
     display: flex;
-    height: 90%;
-    width: 90%;
+    column-gap: 3em;
     border: 1px solid red;
     justify-content: space-around;
     align-self: center;
@@ -90,11 +156,11 @@
   .productsWrapper {
     display: flex;
     flex-direction: column;
-    min-height: 600px;
-    width: 350px;
+    height: 700px;
+    width: 355px;
     align-self: center;
     border: 3px solid #6ba7ff;
-    border-radius: 25px;
+    border-radius: 20px;
   }
   
   .title {
@@ -103,12 +169,13 @@
     padding: 0.5em;
   }
   
-  p {
+  .title p {
     display: flex;
     justify-content: center;
     margin: 0;
     padding: 5px;
     width: 90%;
+    font-size: larger;
   }
   
   .refresh {
@@ -122,6 +189,44 @@
     display: flex;
     justify-content: center;
     flex-wrap: wrap;
-    
+    min-height: 450px;
+    max-height: 450px;
+    overflow-y: scroll;
+    border-top: 3px solid #6ba7ff;
+    border-bottom: 3px solid #6ba7ff;
+  }
+  
+  .productsSummary {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    overflow-y: scroll;
+  }
+  
+  .productsSummary p {
+    margin: 0;
+    padding: 0.3em;
+    font-size: large;
+    border-bottom: 1px solid gray;
+  }
+  
+  .productsToUpdate {
+    padding: 0.3em;
+  }
+  
+  .productActions {
+    display: flex;
+    justify-content: flex-end;
+    padding: 0.5em;
+    border-top: 3px solid #6ba7ff;
+    column-gap: 0.5em;
+  }
+  
+  button {
+    padding: 0.3em;
+    font-size: 1.1em;
+    background-color: lightgray;
+    border: none;
+    border-radius: 5px;
   }
 </style>
